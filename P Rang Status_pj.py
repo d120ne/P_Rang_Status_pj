a4edca50b260d5c978de38f6956e83c91d046b04
d120ne <148317471+d120ne@users.noreply.github.com>
2023-10-18 12:14:16 +0300

    Add def punch, hp

---
+++ P Rang Status_pj.py
@@ -0,0 +1,51 @@
+class Ultrakill_character:
+
+    def __init__(self, name, hp, ):
+        self.name = name
+        self.hp = 1
+
+
+    def salute(name):
+        if name == 'Minos Prime':
+            print('Ahh… Free at last. O Gabriel. Now dawns thy reckoning, and thy gore shall glisten before the temples '
+                  'of man. Creature of steel… My gratitude upon thee for my freedom, but the crimes thy kind have'
+                  ' committed against humanity are NOT forgotten. And thy punishment… is DEATH.')
+        elif name == 'V2':
+            print('110010011010011100101')
+        elif name == 'Gabriel':
+            print('Machine, turn back now. The layers of this palace are not for your kind. Turn back, or you will be'
+                  ' crossing the will of God' + ' ...Your choice is made..')
+
+
+    def punch_reaction():
+
+
+    def punch(self, other, hp):
+        if hp <= 0:
+            print("Can't punch character is already dead")
+        else:
+             other.hp -= 1
+        if other.hp <= 0:
+#            punch_reaction
+
+
+#def ordering(name):
+#    if hp = max:
+#        salution()
+#    other = name
+
+
+class King_Minos(Ultrakill_character):  #    Minos Prime
+
+
+class Judge_of_Hell(Ultrakill_character):   #    Gabriel
+
+
+class Supreme_Machine(Ultrakill_character):    #   V1, V2
+
+
+class Greater_Machine(Ultrakill_character):    #    Swordsmachine
+
+
+#class Apostate_of_Hate(Ultrakill_character):    #   Gabriel_p2
+
