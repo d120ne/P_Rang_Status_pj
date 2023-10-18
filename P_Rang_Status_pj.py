91870fee0e1228797045b45181d44eed65dcaf33
d120ne <148317471+d120ne@users.noreply.github.com>
2023-10-18 10:14:07 +0300

    Just add file, not comitted

---
+++ P_Rang_Status_pj.py
@@ -0,0 +1,32 @@
+class Ultrakill_character:
+
+    def __init__(self, name, hp, ):
+        self.name = name
+        self.hp = hp
+        self.max_hp = hp
+
+
+King_Minos = Ultrakill_character('Minos Prime', hp)
+
+Supreme_Machine = Ultrakill_character('V2', hp)
+
+Judge_of_Hell = Ultrakill_character('Gabriel', hp)
+
+
+# Apostate_of_Hate = Ultrakill_character('Gabriel_p2', hp)
+
+
+def salution(name):
+    if name = 'Minos Prime':
+        print(
+            'Ahh… Free at last. O Gabriel. Now dawns thy reckoning, and thy gore shall glisten before the temples of man. Creature of steel… My gratitude upon thee for my freedom, but the crimes thy kind have committed against humanity are NOT forgotten. And thy punishment… is DEATH.')
+    elif name = 'V2':
+        print('110010011010011100101')
+    elif name = 'Gabriel':
+        print(
+            'Machine, turn back now. The layers of this palace are not for your kind. Turn back, or you will be crossing the will of God' + 'Your choice is made')
+
+
+def ordering(name):
+    if hp = max:
+        salution(
